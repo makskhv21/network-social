59bce8a630090b85ce7ad8f3be2fd75e5f120092
repo <khvits21@gmail.com>
2025-01-29 14:20:22 +0200

    feat: Add project in express

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const multer = require('multer');
+
+const uploadDestination = 'uploads';
+
+// Показати де є зберігається файли
+const storage = multer.diskStorage({
+  destination: uploadDestination,
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const uploads = multer({ storage: storage });
+
+router.get('/register', (req, res) => {
+  res.send('Register');
+});
+
+module.exports = router;
