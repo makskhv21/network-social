9491f20386b26444e431a9596d899d902bf69dfb
repo <khvits21@gmail.com>
2025-02-03 19:29:47 +0200

    feat: Add login and register pages

--- web/src/app/store.ts
+++ web/src/app/store.ts
@@ -1,17 +1,24 @@
-import type { Action, ThunkAction } from "@reduxjs/toolkit"
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
+import { api } from "./services/api"
+import auth from "../features/user/userSlice"
+import { listenerMiddleware } from "../middleware/auth"
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [api.reducerPath]: api.reducer,
+    auth,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(api.middleware)
+      .prepend(listenerMiddleware.middleware),
 })
 
-
-export type AppStore = typeof store
+export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = AppStore["dispatch"]
-export type AppThunk<ThunkReturnType = void> = ThunkAction<
-  ThunkReturnType,
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
   RootState,
   unknown,
-  Action
+  Action<string>
 >
